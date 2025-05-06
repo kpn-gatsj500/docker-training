625ec63ef5e5fd7f4573bbf99963eb142768899c
Thom <gatsj500@cura.kpn.org>
2025-05-06 11:54:49 +0200

    Renamed flask to backend and lighttpd to frontend

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Replace environment variables in the config file
-envsubst '${LIGHTTPD_NAME} ${LIGHTTPD_PORT} ${FLASK_NAME} ${FLASK_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${FRONTEND_NAME} ${FRONTEND_PORT} ${BACKEND_NAME} ${BACKEND_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Execute the original entrypoint with the passed arguments
 exec /docker-entrypoint.sh "$@"
