82ec49f48eb242d615730dc232ffb6f8a93d727b
Thom <gatsj500@cura.kpn.org>
2025-05-06 15:23:35 +0200

    Oops

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Step 1: Convert standalone $ variables (not in ${}) to a temporary placeholder
-sed -E 's/(\$)([a-zA-Z_]+)/###ENVSUBSTNOTOUCH###\2/g' /etc/nginx/conf.d/nginx.conf > /tmp/temp_step1.conf
+sed -E 's/(\$)([a-zA-Z_]+)/###ENVSUBSTNOTOUCH###\2/g' /etc/nginx/conf.d/default.conf > /tmp/temp_step1.conf
 echo "Step 1 results saved to /tmp/temp_step1.conf"
 
 # Step 2: Run envsubst to replace ${} variables
@@ -13,8 +13,8 @@ sed 's/###ENVSUBSTNOTOUCH###/$/g' /tmp/temp_step2.conf > /tmp/temp_step3.conf
 echo "Final result saved to final_output.conf"
 
 # Step 4: Move result back
-cp /tmp/temp_step3.conf /etc/nginx/conf.d/nginx.conf
-echo "Final result moved to /etc/nginx/conf.d/nginx.conf"
+cp /tmp/temp_step3.conf /etc/nginx/conf.d/default.conf
+echo "Final result moved to /etc/nginx/conf.d/default.conf"
 
 # I could clean it up, but nah
 
